829e26cb8ba605f6afb222bdfee0e38ec386660e
unknown <er.rajbatth@gmail.com>
2020-09-23 13:13:49 -0400

    Required Routes are created

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Buyer\BuyerController;
+use App\Http\Controllers\Category\CategoryController;
+use App\Http\Controllers\Product\ProductController;
+use App\Http\Controllers\Seller\SellerController;
+use App\Http\Controllers\Transaction\TransactionController;
+use App\Http\Controllers\User\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,3 +19,21 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Buyers
+Route::apiResource('buyers', BuyerController::class, ['only' => ['index', 'show']]);
+
+// Categories
+Route::apiResource('categories', CategoryController::class);
+
+// Products
+Route::apiResource('products', ProductController::class, ['only' => ['index', 'show']]);
+
+// Sellers
+Route::apiResource('sellers', SellerController::class, ['only' => ['index', 'show']]);
+
+// Transactions
+Route::apiResource('transactions', TransactionController::class, ['only' => ['index', 'show']]);
+
+// Users
+Route::apiResource('users', UserController::class);
