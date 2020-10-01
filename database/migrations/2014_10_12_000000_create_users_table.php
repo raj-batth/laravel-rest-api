6318e9e68e3e606e6651c8cbaba393912d484067
unknown <er.rajbatth@gmail.com>
2020-10-01 13:49:34 -0400

    Changed Migrations to add deleted_at column

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,6 +25,7 @@ class CreateUsersTable extends Migration
             $table->string('verification_token')->nullable();
             $table->boolean('admin')->default(User::REGULAR_USER);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
